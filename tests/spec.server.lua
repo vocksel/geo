1dbc81ce3b4924ebc9b4620cb59c4859113d17fd
David Minnerly <me@vocksel.com>
2021-08-22 12:31:50 -0700

    Fix CI?

--- tests/spec.server.lua
+++ tests/spec.server.lua
@@ -1,4 +1,6 @@
-local TestEZ = require(script.Parent.TestEZ)
+-- The absolute path to TestEZ must be used so that this script can work in
+-- Studio normally, and when using run-in-roblox in CI.
+local TestEZ = require(game.ServerScriptService.Tests.TestEZ)
 
 TestEZ.TestBootstrap:run({
 	game.ReplicatedStorage.Geo,
