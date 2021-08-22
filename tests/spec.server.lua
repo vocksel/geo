fa9ece4fffab3fef6aecca34d7bde73674eb17a0
David Minnerly <me@vocksel.com>
2021-08-22 13:06:12 -0700

    Revise comment

--- tests/spec.server.lua
+++ tests/spec.server.lua
@@ -1,5 +1,5 @@
--- The absolute path to TestEZ must be used so that this script can work in
--- Studio normally, and when using run-in-roblox in CI.
+-- The absolute path to TestEZ must be used so that this script can work with
+-- run-in-roblox in CI.
 local TestEZ = require(game.ServerScriptService.Tests.TestEZ)
 
 TestEZ.TestBootstrap:run({
